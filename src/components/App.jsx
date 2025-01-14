ac18d17142485f46a83c06ada65613eb7990ff0b
Julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-01-14 17:59:16 +0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,10 @@ import SearchBar from "./SearchBar";
 class NoteApp extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             notes : getInitialData(),
             searchTitle : ''
-        }
-
+        };
         this.onDeleteHandler = this.onDeleteHandler.bind(this);
         this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
         this.onArchivedHandler = this.onArchivedHandler.bind(this);
@@ -22,16 +20,16 @@ class NoteApp extends React.Component {
     onDeleteHandler(id) {
         const notes = this.state.notes.filter((note) => note.id !== id);
         this.setState({ notes });
-    }
+    };
 
     onArchivedHandler(id) {
         this.setState((prevState) => {
             return {
                 notes : prevState.notes.map((note) => 
                 note.id === id ? {...note, archived : !note.archived} : note)
-            }
+            };
         })
-    }
+    };
 
     onAddNoteHandler({ title, body, createdAt, archived }) {
         this.setState((prevState) => {
@@ -46,18 +44,17 @@ class NoteApp extends React.Component {
                         archived 
                     }
                 ]
-            }
-        })
-
-    }
+            };
+        });
+    };
 
     onSearchBarChangeHandler(searchedTitle) {
         this.setState(() => {
             return{
                 searchTitle : searchedTitle.trim().toLowerCase()
             }
-        })
-    }
+        });
+    };
 
     render() {
         const activeNotes = this.state.notes.filter((note) => !note.archived);
@@ -65,39 +62,39 @@ class NoteApp extends React.Component {
         const searchedNotes = this.state.searchTitle ? 
                               (this.state.notes.filter((note) => note.title.toLowerCase().includes(this.state.searchTitle.toLowerCase())))
                               : null;
+
         return(
             <div className="note-app">
                 <div className="note-app__header">
                     <h1>Notes</h1>
-                    <SearchBar onChange = {this.onSearchBarChangeHandler}/>
+                    <SearchBar onChange = { this.onSearchBarChangeHandler }/>
                 </div>
                 <div className="note-app__body">
-                    <NoteForm addNote= {this.onAddNoteHandler}/>
+                    <NoteForm addNote= { this.onAddNoteHandler }/>
                     <article className="note-app__body__active">
                         <h2>Catatan Aktif</h2>
                         {
-                            searchedNotes && searchedNotes > 0 ? (<NoteList notes={searchedNotes.filter((note => !note.archived))} onDelete={this.onDeleteHandler} onArchived={this.onArchivedHandler}/>) 
-                            : searchedNotes && searchedNotes === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>)
-                            : activeNotes.length === 0  ? (<p className="note-list__empty-message">Tidak ada Catatan</p>) 
-                            : (<NoteList notes={activeNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchivedHandler} />)
+                            searchedNotes !== null ?
+                            (searchedNotes.length === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>)
+                            : (<NoteList notes={ searchedNotes.filter((note => !note.archived)) } onDelete={ this.onDeleteHandler } onArchived={ this.onArchivedHandler }/>) )
+                            : activeNotes.length  === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>) 
+                            :<NoteList notes={activeNotes} onDelete={ this.onDeleteHandler } onArchived={ this.onArchivedHandler } />
                         }
-                        
-                
                     </article>
+                    
                     <article className="note-app__body__archived">
                         <h2>Arsip Catatan</h2> 
                         {
-                            searchedNotes && searchedNotes > 0 ? (<NoteList notes={searchedNotes.filter((note => !note.archived))} onDelete={this.onDeleteHandler} onArchived={this.onArchivedHandler}/>) 
-                            : searchedNotes && searchedNotes === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>)
-                            : activeNotes.length === 0  ? (<p className="note-list__empty-message">Tidak ada Catatan</p>) 
-                            : (<NoteList notes={archivedNotes} onDelete={this.onDeleteHandler} onArchived={this.onArchivedHandler} />)
+                            searchedNotes !== null ?
+                            (searchedNotes.length === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>)
+                            : (<NoteList notes={ searchedNotes.filter((note => note.archived)) } onDelete={ this.onDeleteHandler } onArchived={ this.onArchivedHandler }/>)) 
+                            : archivedNotes.length === 0 ? (<p className="note-list__empty-message">Tidak ada Catatan</p>)
+                            : <NoteList notes={ archivedNotes } onDelete={ this.onDeleteHandler } onArchived={ this.onArchivedHandler } />
                         }
-                        
                     </article>
                 </div>
             </div>
-        )
-    }
-}
-
+        );
+    };
+};
 export default NoteApp;
